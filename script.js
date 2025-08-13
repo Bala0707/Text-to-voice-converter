8ea744645d759c99352a20677e9c9a9812ec4e74
Bala0707 <bala2342004@gmail.com>
2025-08-13 12:33:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let textBox = document.getElementById(`textbox`);
+let button = document.getElementById(`button`);
+
+//when button click
+button.addEventListener("click",() => { 
+    let text = textBox.value.trim();   
+    if(text === ""){
+        alert("please enter some text")
+        return;
+    }
+    //create a speech 
+    let speech = new SpeechSynthesisUtterance(text);
+    console.log(speech)
+    speech.lang = select.value;
+    // speak the text
+    window.speechSynthesis.speak(speech);
+})
